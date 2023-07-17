5d493eef3694b44e6e7fb46b15800604b692cb87
Stormblessed <carlomanid@gmail.com>
2023-07-17 15:46:25 +0300

    Create homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
+import styles from "./Homepage.module.css";
+
 export default function Homepage() {
-    return <div>home</div>
+    return <main className={styles.homepage}>
+        <Nav/>
+        <section>
+            <h1>Traveling around the world? <br/> MapApp will help you</h1>
+            <h2>The application allows you to leave notes about cities so that you can remember your great (imaginary) travels.</h2>
+            <Link to="app" className="btn-link">Start</Link>
+        </section>
+    </main>
 }
