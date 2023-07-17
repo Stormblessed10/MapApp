2cd01cfd56af5446ad2821394ae137469b2be9ae
Stormblessed <carlomanid@gmail.com>
2023-07-17 16:45:19 +0300

    Create price page

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,3 +1,15 @@
+import Nav from "../components/Nav";
+import styles from "./Price.module.css";
+
 export default function Price() {
-    return <div>price</div>
+    return <main className={styles.price}>
+        <Nav/>
+        <section>
+            <div>
+                <h2>Simple pricing. <br/> Just $9/month.</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel labore mollitia iusto. Recusandae quos provident, laboriosam fugit voluptatem iste.</p>
+            </div>
+            <img src="img-2.jpg" alt="price"/>
+        </section>
+    </main>
 }
