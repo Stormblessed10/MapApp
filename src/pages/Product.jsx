a08f68194d12e9cd3c388c0b07ad52d1047ccd3a
Stormblessed <carlomanid@gmail.com>
2023-07-17 16:39:56 +0300

    Create product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,15 @@
+import Nav from "../components/Nav";
+import styles from "./Product.module.css";
+
 export default function Product() {
-    return <div>prod</div>
+    return <main className={styles.product}>
+        <Nav/>
+        <section>
+            <img src="img-1.jpg" alt="product"/>
+            <div>
+                <h2>About WorldWide</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel labore mollitia iusto. Recusandae quos provident, laboriosam fugit voluptatem iste.</p>
+            </div>
+        </section>
+    </main>
 }
