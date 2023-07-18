4df719302006dfa50c68a2d55d0b23f677af967e
Stormblessed <carlomanid@gmail.com>
2023-07-18 17:03:03 +0300

    Add props and JSX markup

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,7 +1,21 @@
 import styles from "./Countries.module.css";
+import Loader from "./Loader";
+import Message from "./Message";
 
 export default function Countries({ isLoading, cityList }) {
-    
+    if (isLoading) return <Loader/>;
 
-    return 
+    if (!cityList.length) {
+        return <Message message="It's empty here..."/>;
+    }
+
+    const countries = cityList.reduce((acc, cur) =>{
+        if (!acc.map(el => el.country).includes(cur.country)) return [...acc, {country: cur.country, emoji: cur.emoji, id: cur.id}];
+        return acc;
+    }, []);
+    console.log(countries);
+
+    return <ul className={styles.countries}>
+        {countries.map(country => <li key={country.id}><span className={styles.abbrev}>{country.emoji}</span><span className={styles.name}>{country.country}</span></li>)}
+    </ul>
 }
