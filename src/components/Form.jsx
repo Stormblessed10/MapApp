6b8cb5124fbdb4012031261ad6693bbdb8d74da3
Stormblessed <carlomanid@gmail.com>
2023-07-18 22:31:43 +0300

    Add JSX markup and states

---
+++ src/components/Form.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import styles from "./Form.module.css";
+
+export default function Form() {
+    const [cityName, setCityName] = useState("");
+    const [country, setCountry] = useState("");
+    const [visitDate, setVisitDate] = useState("");
+    const [note, setNote] = useState("");
+
+    return <form className={styles.form}>
+            <div>
+                <label>City name</label>
+                <input type="text" onChange={(e) => setCityName(e.target.value)} value={cityName}/>
+                <span>{country}</span>
+            </div>
+            <div>
+                <label>Visit date</label>
+                <input type="text" onChange={(e) => setVisitDate(e.target.value)} value={visitDate}/>
+            </div>
+            <div>
+                <label>Some notes about your trip</label>
+                <textarea rows={3} onChange={(e) => setNote(e.target.value)} value={note}/>
+            </div>
+            <button className="btn">ADD</button>
+        </form> 
+}
