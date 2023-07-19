7daeba495b91857f5b6f68a31653abda149b8b2f
Stormblessed <carlomanid@gmail.com>
2023-07-19 20:19:39 +0300

    Remove props and add provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import Product from "./pages/product";
 import Homepage from "./pages/Homepage";
 import Price from "./pages/Price";
@@ -11,23 +10,26 @@ import Cities from "./components/Cities";
 import Countries from "./components/Countries";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CitiesProvider } from "./context/CitiesContext";
 
 export default function App() {
 
-  return <BrowserRouter>
-  <Routes>
-      <Route index element={<Homepage/>}/>
-      <Route path="product" element={<Product/>}/>
-      <Route path="price" element={<Price/>}/>
-      <Route path="login" element={<Login/>}/>
-      <Route path="app" element={<AppLayout/>}>
-        <Route index element={<Navigate replace to="cities"/>}/>
-        <Route path="cities" element={<Cities cityList={cityList} isLoading={isLoading}/>}/>
-        <Route path="cities/:id" element={<City cityList={cityList}/>}/>
-        <Route path="countries" element={<Countries cityList={cityList} isLoading={isLoading}/>}/>
-        <Route path="form" element={<Form/>}/>
-     </Route>
-      <Route path="*" element={<PageNotFound/>}/>
-  </Routes>
-</BrowserRouter>
+  return <CitiesProvider>
+    <BrowserRouter>
+    <Routes>
+        <Route index element={<Homepage/>}/>
+        <Route path="product" element={<Product/>}/>
+        <Route path="price" element={<Price/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="app" element={<AppLayout/>}>
+          <Route index element={<Navigate replace to="cities"/>}/>
+          <Route path="cities" element={<Cities/>}/>
+          <Route path="cities/:id" element={<City/>}/>
+          <Route path="countries" element={<Countries/>}/>
+          <Route path="form" element={<Form/>}/>
+        </Route>
+        <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+</CitiesProvider>
 }
