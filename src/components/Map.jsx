51b35b2f13f43206bc797fd3033b7ea5c0b8ed32
Stormblessed <carlomanid@gmail.com>
2023-07-19 22:29:24 +0300

    Add map markup

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,26 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 export default function Map() {
     const navigate = useNavigate();
+    const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
 
+    // const position = [searchParams.get("lat"), searchParams.get("lng")];
+    
     return <section className={styles.map} onClick={() => navigate("form")}>
-
+        <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={mapPosition}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+            </Marker>
+        </MapContainer>
     </section>
 }
