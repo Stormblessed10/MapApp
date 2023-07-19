76b64557128e95ae71875b92d69ee7b81c7c7d32
Stormblessed <carlomanid@gmail.com>
2023-07-19 23:08:22 +0300

    Fix icon display

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,6 +3,17 @@ import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useState } from "react";
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 export default function Map() {
     const navigate = useNavigate();
     const [mapPosition, setMapPosition] = useState([40, 0]);
@@ -10,8 +21,8 @@ export default function Map() {
 
     // const position = [searchParams.get("lat"), searchParams.get("lng")];
     
-    return <section className={styles.map} onClick={() => navigate("form")}>
-        <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false}>
+    return <section className={styles["map-container"]} onClick={() => navigate("form")}>
+        <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={false}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
