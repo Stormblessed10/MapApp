2dd24ad18fe4645b4c0f37d644c8328825d52ef2
Stormblessed <carlomanid@gmail.com>
2023-07-20 15:48:18 +0300

    Add a side effect that sets city name and change useState's to useReducer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,67 @@
-import { useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../Hooks/useUrlPosition";
+
+const URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
+const initState = {
+    cityName: "",
+    country: "",
+    visitDate: new Date(),
+    note: "",
+    isLoadingGeo: false
+};
+function reducer(state, action) {
+    switch(action.type) {
+        case "setCityName":
+            return {...state, cityName: action.payload}
+        case "setCountry":
+            return {...state, country: action.payload}
+        case "setVisitDate":
+            return {...state, visitDate: action.payload}
+        case "setNote":
+            return {...state, note: action.payload}
+        case "setIsLoadingGeo":
+            return {...state, isLoadingGeo: !state.isLoadingGeo}
+    }
+}
 
 export default function Form() {
-    const [cityName, setCityName] = useState("");
-    const [country, setCountry] = useState("");
-    const [visitDate, setVisitDate] = useState(new Date());
-    const [note, setNote] = useState("");
+    const [{cityName, country, visitDate, note, isLoadingGeo}, dispatch] = useReducer(reducer, initState);
+    const {lat, lng} = useUrlPosition();
+
+    useEffect(() => {
+        async function fetching() {
+            try {
+                dispatch({type: "setIsLoadingGeo"});
+                const res = await fetch(`${URL}?latitude=${lat}&longitude=${lng}`);
+                const data = await res.json();
+                dispatch({type: "setCityName", payload: data.city || data.locality});
+                dispatch({type: "setIsLoadingGeo", payload: data.countryName});
+            } catch(err) {
+                console.error(err);
+            } finally {
+                dispatch({type: "setIsLoadingGeo"});
+            }
+        }
+        fetching();
+    }, [lat, lng])
 
     return <form className={styles.form}>
             <div>
                 <label>City name</label>
-                <input type="text" onChange={(e) => setCityName(e.target.value)} value={cityName}/>
+                <input type="text" onChange={(e) => dispatch({type: "setCityName", payload: e.target.value})} value={cityName}/>
                 <span>{country}</span>
             </div>
             <div>
                 <label>Visit date</label>
-                <input type="text" onChange={(e) => setVisitDate(e.target.value)} value={visitDate}/>
+                <input type="text" onChange={(e) => dispatch({type: "setVisitDate", payload: e.target.value})} value={visitDate}/>
             </div>
             <div>
-                <label>Some notes about your trip</label>
-                <textarea rows={3} onChange={(e) => setNote(e.target.value)} value={note}/>
+                <label>Some notes about your visit to {cityName}</label>
+                <textarea rows={3} onChange={(e) => dispatch({type: "setNote", payload: e.target.value})} value={note}/>
             </div>
             <div className={styles.btns}>
                 <Button type="primary">ADD</Button>
