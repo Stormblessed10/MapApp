5bbca70f762ded1924d4c8cf6a78b162a25abcd6
Stormblessed <carlomanid@gmail.com>
2023-07-24 22:15:57 +0300

    Add loader for whole page

---
+++ src/components/LoaderPage.jsx
@@ -0,0 +1,6 @@
+import Loader from "./Loader";
+import styles from "./LoaderPage.module.css";
+
+export default function LoaderPage() {
+    return <div className={styles.loaderPage}><Loader/></div>
+}
